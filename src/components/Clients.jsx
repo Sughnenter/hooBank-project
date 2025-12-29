3005936a6ca6c53287aaa22adaa1300f2b5f91da
shammah <shamaj2006@gmail.com>
2025-12-29 22:07:52 +0100

    finised testimonial, updated title and logo, started clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../styles'
 
 function Clients ()  {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) =>(
+          <div key={client.id}>
+            <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
